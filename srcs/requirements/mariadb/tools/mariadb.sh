55bedd907699f7c22f7a6f906d63a2d1db49b9b6
Frederic DUCROT <frederic.ducrot@hotmail.com>
2026-07-06 12:14:13 +0000

    MariaDB service complete with secrets and named volume

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+
+	DB_PASSWORD=$(cat /run/secrets/db_password)
+	DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+
 	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 	cat > /tmp/init.sql <<EOF
-CREATE DATABASE $MYSQL_DATABASE;
-CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 EOF
 else
